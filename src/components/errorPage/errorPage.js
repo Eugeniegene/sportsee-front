720e9ed143fe65727419ab308ba5d7235fdec56d
Eugénie <eugenie.philippe@hotmail.fr>
2022-11-06 12:55:58 +0100

    minor correction

--- src/components/errorPage/errorPage.js
+++ src/components/errorPage/errorPage.js
@@ -3,7 +3,7 @@ import "./errorPage.css"
 const ErrorPage = () => {
     return (
       <div className='errorPage'>
-        <i class="fa-solid fa-triangle-exclamation fa-5x"></i>
+        <i className="fa-solid fa-triangle-exclamation fa-5x"></i>
         <h1> Erreur </h1>
         <span> L'utilisateur demandé n'existe pas. </span>
       </div>
